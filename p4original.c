958e90c7444a19416a86aef5705c01bef10b68e9
Maniiishaaaaaa <93110654+Maniiishaaaaaa@users.noreply.github.com>
2022-01-03 06:19:22 +0000

    p4

---
+++ p4original.c
@@ -0,0 +1,42 @@
+
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter three numbers\n");
+  scanf("%d", &a);
+  return a;
+}
+int calc(int m,int n,int o)
+{
+  int large;
+  if(m>n)
+  {
+    if(m>o)
+    large=m;
+    else
+    large=0;
+    }
+    else
+    {
+      if(n>o)
+      large=n;
+      else
+      large=o;
+    }
+    return large;
+}
+int output(int p)
+{
+  printf("largest number is %d",p);
+}
+int main()
+{
+  int a,b,c,s;
+  a=input();
+  b=input();
+  c=input();
+  s=calc(a,b,c);
+  output(s);
+  return 0;
+}
